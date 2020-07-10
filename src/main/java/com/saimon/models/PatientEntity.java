08bf8a05eceae3a22bce2b00b129575c9e4eb95b
Saimon <saimon.ctg@gmail.com>
2020-07-10 20:03:04 +0600

    Lombok added

--- src/main/java/com/saimon/models/PatientEntity.java
+++ src/main/java/com/saimon/models/PatientEntity.java
@@ -1,13 +1,28 @@
 package com.saimon.models;
 
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 
 //@XmlRootElement  // for generating result in xml format. eta dile o chole abar na dile problem nai.
-@Entity(name = "patient_info")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "patient_info")
 public class PatientEntity {
 
     @Id
@@ -30,64 +45,4 @@ public class PatientEntity {
     @NotNull
     private String patientEmail;
 
-    public PatientEntity() {
-    }
-
-//    public PatientEntity(int patientId, String patientName, String patientAddress, int patientAge, String patientContactNo, String patientEmail) {
-//        super();
-//        this.patientId=patientId;
-//        this.patientName=patientName;
-//        this.patientAddress=patientAddress;
-//        this.patientAge=patientAge;
-//        this.patientContactNo=patientContactNo;
-//        this.patientEmail=patientEmail;
-//    }
-
-    public int getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(int patientId) {
-        this.patientId = patientId;
-    }
-
-    public String getPatientName() {
-        return patientName;
-    }
-
-    public void setPatientName(String patientName) {
-        this.patientName = patientName;
-    }
-
-    public String getPatientAddress() {
-        return patientAddress;
-    }
-
-    public void setPatientAddress(String patientAddress) {
-        this.patientAddress = patientAddress;
-    }
-
-    public int getPatientAge() {
-        return patientAge;
-    }
-
-    public void setPatientAge(int patientAge) {
-        this.patientAge = patientAge;
-    }
-
-    public String getPatientContactNo() {
-        return patientContactNo;
-    }
-
-    public void setPatientContactNo(String patientContactNo) {
-        this.patientContactNo = patientContactNo;
-    }
-
-    public String getPatientEmail() {
-        return patientEmail;
-    }
-
-    public void setPatientEmail(String patientEmail) {
-        this.patientEmail = patientEmail;
-    }
 }
